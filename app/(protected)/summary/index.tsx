cc427770517af23d2a71bb2a4f4bc35722ae2576
jon <zhenagajo234@gmail.com>
2024-06-11 10:25:28 +0700

    feat : add onPress for summary button

--- app/(protected)/summary/index.tsx
+++ app/(protected)/summary/index.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
 
 const SummaryPage = () => {
+
+    const fetchParams = useLocalSearchParams();
+    
+
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ fontWeight: "bold", fontSize: 20 }}>Page In Progress</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>WHAT THE SIGMA</Text>
         </View>
     );
 };
